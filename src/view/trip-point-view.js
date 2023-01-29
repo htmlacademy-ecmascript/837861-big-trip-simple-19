8b8443d6b180be6b71ae3ef745b2e3de23b065cf
Evgeny Korolev <mts9832899777@gmail.com>
2023-01-29 23:08:54 +0700

    add point-presenter

--- src/view/trip-point-view.js
+++ src/view/trip-point-view.js
@@ -60,28 +60,14 @@ const createPointTemplate = (point) => {
     </li>`;
 };
 
-// export default class PointView extends AbstractView {
-//   // export default class PointView {
-//   //   #element = null;
-//   #point = null;
-
-//   constructor({ point }) {
-//     super();
-//     this.#point = point;
-//   }
-
-//   get template() {
-//     return createPointTemplate(this.#point);
-//   }
-// }
 export default class PointView extends AbstractView {
   #point = null;
   #handleRollupBtnClick = null;
 
-  constructor({ point, onRollupBtnClick }) {
+  constructor({ point, onRollupButtonClick }) {
     super();
     this.#point = point;
-    this.#handleRollupBtnClick = onRollupBtnClick;
+    this.#handleRollupBtnClick = onRollupButtonClick;
 
     this.element.querySelector('.event__rollup-btn')
       .addEventListener('click', this.#rollupBtnClickHandler);
